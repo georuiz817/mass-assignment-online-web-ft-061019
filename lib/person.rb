fdfb39249d2bc5c60976718358e6b6cd545b652e
George Ruiz <georuiz817@gmail.com>
2019-07-03 23:33:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width 
-def initialize(attributes)
-  attributes = {:name, :birthday, :hair_color, :eye_color, :height, 
+def initialize(avi_attributes)
+  avi_attributes = {:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width} 
 attributes.each {|key, value| self.send (("#{key}="), value)}
