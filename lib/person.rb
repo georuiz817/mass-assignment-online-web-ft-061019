8a71aea711d113cdc1d5ee4dbbe63dfd6a11e841
George Ruiz <georuiz817@gmail.com>
2019-07-03 23:33:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,5 +4,6 @@ def initialize(attributes)
   attributes = {:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width} 
-attributes.each {|key, value| self.send
+attributes.each {|key, value| self.send (("#{key}="), value)}
+end
 end
