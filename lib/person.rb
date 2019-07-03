767c5595daab205c46ffec4e482ab6c2bd8144d3
George Ruiz <georuiz817@gmail.com>
2019-07-03 23:22:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-person_attributes = 
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+def initialize 
+  attributes = [:name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width]
+end
 end
